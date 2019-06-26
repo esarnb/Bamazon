748e67f6ded63d0824088b0dc228de60a25f6d11
Esar Behlum <esarnb@gmail.com>
2019-06-26 16:17:47 -0700

    Added display table function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,6 +16,18 @@ connection.connect(function (err) {
   createDB();
 });
 
+function displayTable(cb) {
+  console.clear();
+  connection.query("SELECT * FROM products", function (err, resp) {
+    if (err) throw err;
+    var table = new Table({
+      head: ["ID", "Item", "Department", "Price", "Stock"]
+    });
+    for (row of resp) { table.push([row.item_id, row.product_name, row.department_name, "$" + row.price.toFixed(2), row.stock_quantity]); }
+    console.log(table.toString()); cb();
+  });
+}
+
 function promptCB(prompt, cb) {
   console.log("\n", prompt); cb();
 }
